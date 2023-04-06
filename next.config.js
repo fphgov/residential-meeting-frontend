eaf754f0afe877fd98cf829092bed9ef53f4a332
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-04-06 17:32:19 +0200

    Fixed next.js config
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
+const { BACKEND_URL, MATOMO_URL, MATOMO_SITE_ID } = process.env
+
 module.exports = async (phase, { defaultConfig }) => {
   /**
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
+    poweredByHeader: false,
+    images: {
+      unoptimized: true,
+    },
+    serverRuntimeConfig: {
+      apiUrl: BACKEND_URL,
+    },
+    publicRuntimeConfig: {
+      matomoUrl: MATOMO_URL,
+      matomoSiteId: MATOMO_SITE_ID,
+    },
     experimental: {
-      images: {
-        unoptimized: true,
-      },
+      forceSwcTransforms: true,
     },
   }
+
   return nextConfig
 }
