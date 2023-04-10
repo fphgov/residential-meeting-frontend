654c6f397fe8b34158fac2cef70d44509b381ebd
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-04-10 15:08:57 +0200

    Add active class in current page
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/section/HeaderSection.js
+++ src/section/HeaderSection.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import headerLogo from '../../public/image/bp-residential-header.svg'
 import HamburgerMenu from '../component/HamburgerMenu'
 
 function HeaderSection({ position, showHeaderLine = false }) {
+  const router = useRouter()
+
   const [fixed, setFixed] = useState(false)
   const [openMenu, setOpenMenu] = useState(false)
+
+  const { asPath } = router
 
   const handleScroll = () => {
     if (window.scrollY >= (window.innerHeight > 767 ? 340 : 220)) {
@@ -52,7 +57,7 @@ function HeaderSection({ position, showHeaderLine = false }) {
                       <li><a href="/#" onClick={toggleMenu}><span>Főoldal</span></a></li>
                       <li><a href="/#" onClick={toggleMenu}><span>Mi a lakógyűlés?</span></a></li>
                       <li><a href="/#" onClick={toggleMenu}><span>Kérdések</span></a></li>
-                      <li><Link href="/" onClick={toggleMenu}><span>Szavazás</span></Link></li>
+                      <li><Link href="/szavazas" className={`${/^\/szavazas/.test(asPath) ? 'active' : ''}`} onClick={toggleMenu}><span>Szavazás</span></Link></li>
                     </ul>
                   </div>
                 </nav>
