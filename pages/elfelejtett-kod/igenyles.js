3afd78e933cf0506ce1cee8b92627f339ebc2d70
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-05-27 18:39:30 +0200

    Change dissemination text
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- pages/elfelejtett-kod/igenyles.js
+++ pages/elfelejtett-kod/igenyles.js
@@ -35,14 +35,8 @@ function NewAuth() {
                     </div>
                     <div className='blue-label-info-box'>
                       <p className='headline'>Ellenőrizd, hogy a bejelentett lakcímed kerületében mikorra várható az utolsó levél érkezése! </p>
-                      <div><b>A levelek terjesztése befejeződött az alábbi kerületekben:</b> I., V., VI.</div>
-                      <div><b>A levelek terjesztése jelenleg folyamatban van az alábbi kerületekben:</b> III., IV., VII., VIII., IX., X., XI., XII., XIII.</div>
-                      <div><b>A terjesztés várható időtartama a kerületekben:</b></div>
-                      <ul>
-                        <li>II., XIV., XV. kerület: 2023. május 19-29.</li>
-                        <li>XVI., XVII. kerület: 2023. május 24. - június 3.</li>
-                        <li>XVIII., XIX., XX., XXI., XXII., XXIII. kerület: 2023. május 26. - június 5.</li>
-                      </ul>
+                      <div><b>A levelek terjesztése befejeződött az alábbi kerületekben:</b> I., IV., V., VI., VIII.</div>
+                      <div><b>A levelek terjesztése jelenleg folyamatban van az alábbi kerületekben:</b> II., III., VII., IX., X., XI., XII., XIII, XIV., XV., XVI., XVII., XVIII., XIX., XX., XXI., XXII., XXIII.</div>
                     </div>
                   </div>
                   <div className="middle-section-point-wrapper">
